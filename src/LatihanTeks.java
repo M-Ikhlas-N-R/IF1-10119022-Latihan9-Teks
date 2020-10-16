8803c7acd24d3b3eb556f30f5a067d3e957ccf5d
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-10-16 09:24:58 +0700

    Add files via upload

---
+++ src/LatihanTeks.java
@@ -0,0 +1,19 @@
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : Latihan menggunakan teks
+ *
+ */
+
+public class LatihanTeks {
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi" + c);
+    }
+}
